33cb99e41a28011e2baef89befe67c37baf89fbc
TheNatan27 <74319000+TheNatan27@users.noreply.github.com>
2023-11-06 00:12:51 +0100

    Simple console log added

--- Config/statikusTest.ts
+++ Config/statikusTest.ts
@@ -7,7 +7,9 @@ type resetAUT = {
 
 export const statikusTest = base.extend({
     resetAUT: async ({}, use) => {
+        console.log('Reset started...')
         await execa('rm', ['-R', './BUILD'])
         await execa('dotnet', ['publish', '-o', './BUILD', './EK7TKN_HFT_2021221.sln'])        
+        console.log('New server installed!')
     }
 })
