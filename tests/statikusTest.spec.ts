a7173edd0219f13122453d2a618f08f2013b6304
TheNatan27 <74319000+TheNatan27@users.noreply.github.com>
2023-11-06 13:38:04 +0100

    Fix dll path

--- tests/statikusTest.spec.ts
+++ tests/statikusTest.spec.ts
@@ -7,7 +7,7 @@ test.describe('Example test', () => {
   test('should allow me to add todo items', async ({page}) => {
     const buildPath = path.join(__dirname, '../EK7TKN_HFT_2021221')
     const installedPath = path.join(__dirname, '../INSTALLED')
-    const dllPath = path.join(installedPath, '../EK7TKN_HFT_2021221.Endpoint.dll')
+    const dllPath = path.join(installedPath, '../INSTALLED/EK7TKN_HFT_2021221.Endpoint.dll')
     console.log(buildPath)
     console.log(installedPath)
     console.log(dllPath)
