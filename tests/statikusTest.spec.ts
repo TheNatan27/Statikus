6c161acfa3fea912997bd0b822640383ec44b496
TheNatan27 <74319000+TheNatan27@users.noreply.github.com>
2023-11-06 14:30:58 +0100

    Extend timeout

--- tests/statikusTest.spec.ts
+++ tests/statikusTest.spec.ts
@@ -21,7 +21,7 @@ test.describe('Example test', () => {
     console.log('This is test 01');
     const ipAddress = process.env.IP_ADDRESS || '192.168.100.8';
 
-    await page.goto('http://localhost:5000/user/read/2', {timeout: 20_000});
+    await page.goto('http://localhost:5000/user/read/2', {timeout: 60_000});
 
     await expect(page).toHaveURL('http://localhost:5000/user/read/2');
 
